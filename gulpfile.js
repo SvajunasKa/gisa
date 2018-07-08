0a828619ca17667488a60d00f530df236130cde2
Svajunas Kaseta <svajunas@aventusgroup.com>
2018-07-08 19:39:47 +0300

    degaiciu svente

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,27 @@ var css = require("gulp-clean-css");
 var rename = require('gulp-rename');
 
 gulp.task("imagemin", function () {
-    gulp.src("images/FullSize/**/*.png")
-        .pipe(imagemin())
-        .pipe(gulp.dest("imagemin"))
+  gulp.src("images/FullSize/**/*.jpg")
+    .pipe(imagemin())
+    .pipe(gulp.dest("imagemin"))
 });
 
 gulp.task("css", function () {
-    gulp.src("assets/css/main.css")
-        .pipe(css())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest("assets/css"))
+  gulp.src("assets/css/main.css")
+    .pipe(css())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest("assets/css"))
 });
 
 gulp.task('watch', function () {
-    gulp.watch('assets/css/main.css', ['css']);
+  gulp.watch('assets/css/main.css', ['css']);
+
 });
 
 
 gulp.task('default', [
-    'css',
-    'watch'
+  'css',
+  'watch',
+  'imagemin'
 ]);
 
